70532a059261b4ebf3e1e5cd9881a48f3959c33f
Sharath <sharathpavan@rehlat.com>
2024-05-19 12:15:33 +0530

    Todo list screen basic design.

--- feature/src/main/java/com/example/feature/TodoListScreen.kt
+++ feature/src/main/java/com/example/feature/TodoListScreen.kt
@@ -1,7 +1,20 @@
 package com.example.feature
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -16,8 +29,45 @@ fun TodoListScreen(
 
 @Composable
 fun TodoListContent() {
-    Text(
-        text = "Hello world",
+//    val todos by viewModel.todoList.collectAsState()
+//    val searchQuery by viewModel.searchQuery.collectAsState()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("TODO List") },
+                actions = {
+                    TextField(
+                        value = "searchQuery",
+                        onValueChange = { /*viewModel.updateSearchQuery(it)*/ },
+                        placeholder = { Text("Search TODOs") }
+                    )
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*viewModel.onAddNewTodoClick()*/ }) {
+                Icon(Icons.Default.Add, contentDescription = "Add TODO")
+            }
+        },
+        content = {
+            if (/*todos.isEmpty()*/true) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("Press the + button to add a TODO item")
+                }
+            } else {
+                LazyColumn {
+//                    items(todos) { todo ->
+//                        Text(todo.name)
+//                    }
+                }
+            }
+        }
     )
 }
 
