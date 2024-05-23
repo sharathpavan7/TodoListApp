e8de495bc9b7378aeda7702de7540f1b283c1eb4
Sharath <sharathpavan@rehlat.com>
2024-05-23 23:51:20 +0530

    Search debounce and error message.

--- feature/src/main/java/com/todo/feature/TodoListScreen.kt
+++ feature/src/main/java/com/todo/feature/TodoListScreen.kt
@@ -1,25 +1,18 @@
 package com.todo.feature
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -29,30 +22,35 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.todo.data.model.Todo
-
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun TodoListScreen(
     navController: NavHostController,
     viewModel: TodoViewModel = hiltViewModel()
 ) {
-    val todoList = viewModel.todoList.collectAsState(initial = emptyList())
-    val searchQuery = viewModel.searchQuery.collectAsState().value
+    val todoList by viewModel.todoList.collectAsState(initial = emptyList())
+    val searchQuery by viewModel.searchQuery.collectAsState()
+    val errorEvent by viewModel.errorEvent.collectAsState()
 
     TodoListContent(
-        navController,
-        todoList = { todoList.value },
+        navController = navController,
+        todoList = { todoList },
         searchQuery = { searchQuery },
-        onSearchChange = { viewModel.setSearchQuery(it) })
+        onSearchChange = { viewModel.setSearchQuery(it) },
+        errorEvent = { errorEvent },
+        clearErrorEvent = { viewModel.clearErrorEvent() }
+    )
 }
-
 
 @Composable
 fun TodoListContent(
     navController: NavHostController,
     todoList: () -> List<Todo>,
     searchQuery: () -> String,
-    onSearchChange: (String) -> Unit
+    onSearchChange: (String) -> Unit,
+    errorEvent: () -> String?,
+    clearErrorEvent: () -> Unit
 ) {
     Scaffold(
         topBar = {
@@ -77,7 +75,6 @@ fun TodoListContent(
                 }
             } else {
                 Column(modifier = Modifier.padding(16.dp)) {
-
                     TextField(
                         modifier = Modifier
                             .fillMaxWidth()
@@ -86,6 +83,22 @@ fun TodoListContent(
                         onValueChange = { onSearchChange(it) },
                         placeholder = { Text(stringResource(R.string.search_todos)) }
                     )
+
+                    errorEvent()?.let {
+                        AlertDialog(
+                            onDismissRequest = { clearErrorEvent() },
+                            title = { Text(stringResource(R.string.error)) },
+                            text = { Text(stringResource(R.string.failed_to_add_todo)) },
+                            confirmButton = {
+                                Button(
+                                    colors = ButtonDefaults.buttonColors(Color.Blue),
+                                    onClick = { clearErrorEvent() }) {
+                                    Text(stringResource(R.string.dismiss))
+                                }
+
+                            }
+                        )
+                    }
 
                     LazyColumn {
                         items(todoList()) {
@@ -104,7 +117,6 @@ fun TodoListContent(
     )
 }
 
-
 @Composable
 @Preview(showBackground = false)
 fun TodoListContentPreview() {
@@ -112,5 +124,8 @@ fun TodoListContentPreview() {
         navController = NavHostController(LocalContext.current),
         todoList = { emptyList() },
         searchQuery = { "" },
-        onSearchChange = {})
+        onSearchChange = {},
+        errorEvent = {""},
+        clearErrorEvent = {}
+    )
 }
--- feature/src/main/java/com/todo/feature/TodoViewModel.kt
+++ feature/src/main/java/com/todo/feature/TodoViewModel.kt
@@ -8,11 +8,7 @@ import com.todo.data.model.Todo
 import com.todo.data.repository.TodoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,8 +17,14 @@ class TodoViewModel @Inject constructor(
     private val repository: TodoRepository
 ) : ViewModel() {
 
+    private val _errorEvent = MutableStateFlow<String?>(null)
+    val errorEvent: StateFlow<String?> = _errorEvent
+
     private val _searchQuery = MutableStateFlow("")
     val searchQuery: StateFlow<String> = _searchQuery
+
+    private val debouncedSearchQuery: StateFlow<String> =
+        _searchQuery.debounce(2000).stateIn(viewModelScope, SharingStarted.Lazily, "")
 
     private val _todo = mutableStateOf("")
     val todo: State<String> = _todo
@@ -34,39 +36,50 @@ class TodoViewModel @Inject constructor(
     val addTodoInProgress: State<Boolean> = _addTodoInProgress
 
     val todoList: StateFlow<List<Todo>> = repository.getTodos()
-        .combine(_searchQuery) { todos, query ->
+        .combine(debouncedSearchQuery) { todos, query ->
             if (query.isEmpty()) {
                 todos
             } else {
-                todos.filter { it.description.contains(query) }
+                todos.filter { it.description.contains(query, ignoreCase = true) }
             }
         }.stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
-
 
     internal fun onTodoValueChange(text: String) {
         _todo.value = text
     }
 
+    internal fun clearErrorEvent() {
+        _errorEvent.value = null
+    }
+
     internal fun setSearchQuery(query: String) {
-        _searchQuery.value = query
+        _searchQuery.update { query }
     }
 
     internal fun addTodo(onComplete: () -> Unit) {
         if (validationSuccess()) {
-            _addTodoInProgress.value = true
             viewModelScope.launch {
-                delay(3000)
-                repository.insert(Todo(description = _todo.value))
-                _addTodoInProgress.value = false
-                onComplete()
+                try {
+                    if (_todo.value.equals("Error", ignoreCase = true)) {
+                        throw IllegalArgumentException()
+                    }
+                    _addTodoInProgress.value = true
+                    delay(3000)
+                    repository.insert(Todo(description = _todo.value))
+                    _addTodoInProgress.value = false
+                    onComplete()
+                } catch (e: Exception) {
+                    _addTodoInProgress.value = false
+                    _errorEvent.update { "Failed to add TODO" }
+                    onComplete()
+                }
             }
         }
     }
 
-    private fun validationSuccess() : Boolean {
+    private fun validationSuccess(): Boolean {
         val todoIsNotEmpty = _todo.value.isNotEmpty()
-        _isTodoError.value = todoIsNotEmpty.not()
+        _isTodoError.value = !todoIsNotEmpty
         return todoIsNotEmpty
     }
-
 }
