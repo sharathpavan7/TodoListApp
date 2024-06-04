39e316754f89d3093bc65972ed2d2b73a8a5bfcb
Sharath <sharathpavan7@gmail.com>
2024-06-04 23:44:16 +0530

    ViewModel intialization changed.

--- app/src/main/java/com/todo/todolistapp/MainActivity.kt
+++ app/src/main/java/com/todo/todolistapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.todo.todolistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,6 +23,9 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: TodoViewModel by viewModels()
+
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +38,7 @@ class MainActivity : ComponentActivity() {
                 ) {
 
                     val navController = rememberNavController()
-                    val viewModel: TodoViewModel = hiltViewModel()
+
                     Scaffold {
                         NavHost(
                             modifier = Modifier.padding(it),
